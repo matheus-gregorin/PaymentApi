91f0a1f01886c6da2d91ca5ff65845f6b79a2a4d
Matheus Gregorin <math.gregorin@gmail.com>
2025-05-02 12:47:29 -0300

    feat: Adjusted location for controllers in project

---
+++ src/main/java/gregorin/math/paymentapi/Application/Controllers/PaymentController.java
@@ -0,0 +1,16 @@
+package gregorin.math.paymentapi.Application.Controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("payments")
+public class PaymentController {
+
+    @GetMapping
+    public String getPayment()
+    {
+        return "Payment";
+    }
+}
