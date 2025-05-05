3037a2eefb15b3c62850b8ef32323efb897a1a55
Matheus Gregorin <math.gregorin@gmail.com>
2025-05-05 15:27:28 -0300

    feat: application clean code and repository patterns in te app

--- src/main/java/gregorin/math/paymentapi/Application/Controllers/PaymentController.java
+++ src/main/java/gregorin/math/paymentapi/Application/Controllers/PaymentController.java
@@ -1,16 +1,28 @@
 package gregorin.math.paymentapi.Application.Controllers;
 
+import gregorin.math.paymentapi.Domain.Entities.UserEntity;
+import gregorin.math.paymentapi.Domain.Repositories.UserRepositoryInterface;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("payments")
 public class PaymentController {
 
+    @Autowired
+    private final UserRepositoryInterface userRepository;
+
+    public PaymentController(UserRepositoryInterface userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @GetMapping
-    public String getPayment()
+    public Optional<UserEntity> getUser()
     {
-        return "Payment";
+        return this.userRepository.findByName("Maria Souza");
     }
 }
