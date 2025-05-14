de219a88424592010cbfa906aaaab63d11cc74cd
Matheus Gregorin <math.gregorin@gmail.com>
2025-05-14 19:02:23 -0300

    feat: add new method for modify user entity in User dto

--- src/main/java/gregorin/math/paymentapi/Application/Mapping/UserMapping.java
+++ src/main/java/gregorin/math/paymentapi/Application/Mapping/UserMapping.java
@@ -1,5 +1,6 @@
 package gregorin.math.paymentapi.Application.Mapping;
 
+import gregorin.math.paymentapi.Domain.Dtos.UserResponseDto;
 import gregorin.math.paymentapi.Domain.Entities.UserEntity;
 import gregorin.math.paymentapi.Infrastructure.Models.UserModel;
 
@@ -10,8 +11,20 @@ public class UserMapping {
             userModel.getUuid(),
                 userModel.getName(),
                     userModel.getEmail(),
-                        userModel.getCreated_at(),
-                            userModel.getUpdated_at()
+                        userModel.getPassword(),
+                            userModel.getCreated_at(),
+                                userModel.getUpdated_at()
+        );
+    }
+
+    public static UserResponseDto mapToDto (UserEntity user)
+    {
+        return new UserResponseDto(
+                user.getUuid(),
+                user.getName(),
+                user.getEmail(),
+                user.getCreated_at(),
+                user.getUpdated_at()
         );
     }
 
